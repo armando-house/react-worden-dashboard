135d0d2c77a3b7a202abfab77f89364e6f1ca451
peacedev89 <112556433+peacedev89@users.noreply.github.com>
2022-05-15 17:05:00 +0100

    ss

--- src/components/MainContent/index.js
+++ src/components/MainContent/index.js
@@ -4,14 +4,4 @@ import Statistics from '../Statistics'
 import Logger from '../Logger'
 import UserStatistics from '../UserStatistics'
 
-export default class MainContent extends Component {
-  render() {
-    return (
-        <Switch>
-          <Route path='/' exact component={Statistics} />
-          <Route path='/logs' component={Logger} />
-          <Route path='/userStatistics/:id' component={UserStatistics} />
-        </Switch>
-    )
-  }
-}
+
